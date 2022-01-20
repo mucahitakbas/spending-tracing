7269744b18fdf91939b521ea1d2b74cd65610755
mucahitakbas <ma.mucahitakbas@gmail.com>
2022-01-20 15:33:43 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SingUp from "./components/SingUp";
+import { Layout, Menu, Breadcrumb } from "antd";
 
+const { Header, Content, Footer } = Layout;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "40px", marginTop: 64 }}
+      >
+        <div
+          className="site-layout-background"
+          style={{ padding: 24, minHeight: 380 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/register"
+                element={<SingUp />}
+              />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Spending Tracing @ Mücahit Akbaş 2022
+      </Footer>
+    </Layout>
   );
 }
 
---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { UserState } from "../types/user";
+import userReducer from "./reducers/userReducer";
+
+interface AppState {
+  user: UserState;
+//   categories: any;
+//   records: any;
+}
+const rootReducer = combineReducers<AppState>({
+  user: userReducer,
+//   categories: () => {},
+//   records: () => {},
+});
+export default rootReducer;
